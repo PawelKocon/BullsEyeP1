cd1feb36aef9e7331240e32b5a925ad67050c787
Karolina Lubieniecka-Kocon <lubieniecka@interia.pl>
2019-09-08 11:53:44 +0200

    Initial Commit

---
+++ BullsEyeP1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullsEyeP1
+//
+//  Created by Karolina Lubieniecka-Kocon on 08/09/2019.
+//  Copyright © 2019 PawelKocon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BullsEyeP1Tests/BullsEyeP1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  BullsEyeP1Tests.swift
+//  BullsEyeP1Tests
+//
+//  Created by Karolina Lubieniecka-Kocon on 08/09/2019.
+//  Copyright © 2019 PawelKocon. All rights reserved.
+//
+
+import XCTest
+@testable import BullsEyeP1
+
+class BullsEyeP1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
